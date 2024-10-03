630b31b81a6ff089658bd511e042a2ac0246023c
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2024-10-03 15:09:44 +0200

    premier commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ProductComponent from './components/ProductComponents.tsx';
+import  CartComponent  from './components/CardComponents.tsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductComponent />
+      <CartComponent />
+    </div>
+  );
+}
+
+export default App;
